6c096204ae69846d8eca1be06c4a3ca8991422a1
Tania <pashkevichtania2004@gmail.com>
2024-02-08 11:15:00 +0300

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3001,
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+      hooks: path.resolve(__dirname, 'src/hooks'),
+      utils: path.resolve(__dirname, 'src/utils'),
+    },
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.svg'],
   },
 });
