ea2ffaaae711fb78695be3d955cdf024e8d611e4
Tania <pashkevich.tatsiana@mbicycle.com>
2024-03-29 11:44:00 +0300

    add npmrc, change configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  important: true,
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@mbicycle/foundation-ui-kit/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue': {
+          '50': '#f0f5fe',
+          '100': '#dde8fc',
+          '200': '#c2d7fb',
+          '300': '#98bff8',
+          '400': '#689df2',
+          '500': '#4579ec',
+          '600': '#2a57e0',
+          '700': '#2748ce',
+          '800': '#253ba8',
+          '900': '#243784',
+          '950': '#1a2351',
+        },
+      },
+    },
   },
   plugins: [],
 };
