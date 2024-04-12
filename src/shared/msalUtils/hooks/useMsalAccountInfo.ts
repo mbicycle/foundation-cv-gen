b37165a783812818bbd46237721be18d82aeda03
Pashkevich Tania <pashkevich.tatsiana@mbicycle.com>
2024-04-12 11:19:42 +0300

    refactor

--- src/shared/msalUtils/hooks/useMsalAccountInfo.ts
+++ src/shared/msalUtils/hooks/useMsalAccountInfo.ts
@@ -1,9 +1,7 @@
-import type { AccountInfo } from "@azure/msal-common"
 import { useAccount, useMsal } from "@azure/msal-react"
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-export const useMsalAccountInfo = (): AccountInfo => {
+export const useMsalAccountInfo = () => {
   const { accounts } = useMsal()
 
-  return useAccount({ username: accounts[0]?.username }) as AccountInfo
+  return useAccount({ username: accounts[0]?.username })
 }
